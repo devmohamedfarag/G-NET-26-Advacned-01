def870ee38abd3bb589d3fa59c1fbd41678a924c
mohamedfarag22-dot <mohamedfarag2272001@gmail.com>
2026-03-16 14:14:26 +0200

    Qussetion02

--- AssignmentAdvancedC#01/Program.cs
+++ AssignmentAdvancedC#01/Program.cs
@@ -5,3 +5,24 @@
 //         It allows you to create a class that can be used with different types without having to write separate code for each type.
 //         Generics provide type safety, code reusability, and performance benefits by allowing you to define a class or method with a placeholder for the type it operates on.
 #endregion
+
+#region Qusetion02
+// Q2: Write a generic class Container<T> with Add and Get methods.
+
+// Answer:
+/* 
+public class Container<T>
+{
+    public T Item { get; private set; }
+
+    public void Add(T item)
+    {
+        Item = item;
+    }
+
+    public T Get()
+    {
+        return Item;
+    }
+}*/
+#endregion
