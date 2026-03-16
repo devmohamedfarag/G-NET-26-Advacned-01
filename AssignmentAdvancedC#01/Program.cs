90613a2be5effbf615b34d6a233a013fce7629c4
mohamedfarag22-dot <mohamedfarag2272001@gmail.com>
2026-03-16 17:17:50 +0200

    Question14

--- AssignmentAdvancedC#01/Program.cs
+++ AssignmentAdvancedC#01/Program.cs
@@ -270,3 +270,26 @@ public class Utility
 }
  */
 #endregion
+
+#region Question14
+// Q14: Write a SafeList<T> that returns default when the index is invalid.
+
+// Answer: 
+/*
+public class SafeList<T>
+{
+    private List<T> items = new List<T>();
+    public void Add(T item)
+    {
+        items.Add(item);
+    }
+    public T Get(int index)
+    {
+        if (index < 0 || index >= items.Count)
+        {
+            return default(T); // Return default value if index is invalid
+        }
+        return items[index];
+    }
+}*/
+#endregion
